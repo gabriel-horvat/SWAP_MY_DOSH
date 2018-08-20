dfe8ad15e36b5d9f7e60a2a1a8573e13b69f7991
Gabe Horvat <realgabrielhorvat@gmail.com>
2018-08-20 18:27:06 +0200

    created rudimentary seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Offer.destroy_all
+Request.destroy_all
+Message.destroy_all
+User.destroy_all
+Review.destroy_all
+
+
+User.create(
+  [
+    {
+      email: "ann@example.com",
+      password: "123456"
+    },
+    {
+      email: "bob@example.com",
+      password: "123456"
+    },
+    {
+      email: "charlie@example.com",
+      password: "123456"
+    }
+  ]
+)
+
+
+puts "user Created!"
+
+
+# requests
+  request_1 = Request.create!(
+    request_currency: "USD",
+    wanted_currency: "EUR",
+    request_amount: 200,
+    wanted_amount: 170,
+    location: "Berlin, Germany",
+    start_date: Date.new(2018,8,29),
+    end_date: Date.new(2018,8,31),
+    status: "accepted",
+    user_id: 1,
+    user: User.all.sample
+  )
+
