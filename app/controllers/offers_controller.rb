cb8441d93ff86a11037bb81d25ec8c909eb6855f
Pascal Mies <pascalniklasmies@gmail.com>
2018-08-21 18:45:23 +0200

    advanced offers

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,10 +1,17 @@
 class OffersController < ApplicationController
-  
+
   def index
     @offers = Offers.all
   end
 
   def create
+    @offer = Offer.new(offer_params)
+     @offer.user = current_user
+    if @offer.save
+      redirect_to offer_path, notice: "Have a chat on the conditions of your swap!"
+    else
+      redirect_to requests_path
+    end
   end
 
   def show
