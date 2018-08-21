a88149e0ffef040ff5a22237e37d20ea0de316f5
Gabe Horvat <realgabrielhorvat@gmail.com>
2018-08-21 11:34:44 +0200

    implement seed file, set up rudimentary index.html.erb request view, request_controller, tested seed

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,43 @@
+class RequestsController < ApplicationController
+
+ def index
+    @requests = Request.all
+  end
+
+  def show
+  end
+
+  def new
+    @request = Request.new
+  end
+
+  def create
+    @request = Request.new(request_params)
+     @request.user = current_user
+    if @request.save
+      redirect_to requests_path(@request), notice: "Your request is now visible to other doshers!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def set_task
+    @request = Request.find(params[:id])
+  end
+
+  def request_params
+    params.require(:request).permit(:request_currency, :wanted_currency, :location, :start_date, :end_date, :request_amount, :user_id)
+  end
+end
