a6f82d18e4d0c3654d3fd0453703642b70fe3741
Gabe Horvat <realgabrielhorvat@gmail.com>
2018-08-22 12:09:15 +0200

    implement basic search by location start and end date

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,7 +1,8 @@
 class Request < ApplicationRecord
+  include PgSearch
+
+    pg_search_scope :search_by_location_and_start_date_and_end_date,
+    against: [ :location, :start_date, :end_date ]
 
   belongs_to :user
-
-
-
 end
