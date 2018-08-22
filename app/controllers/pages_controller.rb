f7b8557338dc93f21c29ffa8f13bef93afbec041
Michael Wong <mail@michaelwongsilversmith.com>
2018-08-22 16:42:02 +0200

    added cards to Landing Page as test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @user = User.all.sample
+    @request = Request.all.sample
+    @requests = Request.all
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_154410) do
+ActiveRecord::Schema.define(version: 2018_08_22_113446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 2018_08_21_154410) do
     t.string "chat_status", default: "offline"
     t.string "photo"
     t.string "description"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
