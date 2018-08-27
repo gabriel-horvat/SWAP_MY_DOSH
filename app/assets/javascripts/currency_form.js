50a21a3b8f408a2c78afe0ac3f5a19aa942433c7
Gabe Horvat <realgabrielhorvat@gmail.com>
2018-08-27 18:49:19 +0200

    form works

--- app/assets/javascripts/currency_form.js
+++ app/assets/javascripts/currency_form.js
@@ -25,7 +25,7 @@ document.querySelector("#input-currency .next").addEventListener("click", (event
 })
 
 
-// click on previous button from quantity currency to input
+//  previous button from quantity currency to input
 document.querySelector("#quantity-currency .previous").addEventListener("click", (event) => {
     document.querySelector("#quantity-currency").classList.remove("activated");
     document.querySelector('#quantity-currency').classList.add("deactivated");
@@ -35,7 +35,7 @@ document.querySelector("#quantity-currency .previous").addEventListener("click",
 
 })
 
-// click on next button to remove first page and show second page
+// from quantity currency to wanted currency
 document.querySelector("#quantity-currency .next").addEventListener("click", (event) => {
     document.querySelector("#quantity-currency").classList.remove("activated");
     document.querySelector('#quantity-currency').classList.add("deactivated");
@@ -46,15 +46,56 @@ document.querySelector("#quantity-currency .next").addEventListener("click", (ev
 
 
 // previous button from wanted currency to quantity currency
+document.querySelector("#wanted-currency .previous").addEventListener("click", (event) => {
+    document.querySelector("#wanted-currency").classList.remove("activated");
+    document.querySelector('#wanted-currency').classList.add("deactivated");
+        document.querySelector("#quantity-currency").classList.remove("deactivated");
+    document.querySelector('#quantity-currency').classList.add("activated");
 
+
+})
 
 // wanted currency to location
+document.querySelector("#wanted-currency .next").addEventListener("click", (event) => {
+    document.querySelector("#wanted-currency").classList.remove("activated");
+    document.querySelector('#wanted-currency').classList.add("deactivated");
+  document.querySelector("#location").classList.remove("deactivated");
+    document.querySelector('#location').classList.add("activated");
+
+})
+
 
 
 // previous button from location to wanted currency
 
+document.querySelector("#location .previous").addEventListener("click", (event) => {
+    document.querySelector("#location").classList.remove("activated");
+    document.querySelector('#location').classList.add("deactivated");
+        document.querySelector("#wanted-currency").classList.remove("deactivated");
+    document.querySelector('#wanted-currency').classList.add("activated");
+
+
+})
+
 
 // location to time
+document.querySelector("#location .next").addEventListener("click", (event) => {
+    document.querySelector("#location").classList.remove("activated");
+    document.querySelector('#location').classList.add("deactivated");
+  document.querySelector("#time").classList.remove("deactivated");
+    document.querySelector('#time').classList.add("activated");
+
+})
+
 
 
 // previous button back from time to location
+document.querySelector("#time .previous").addEventListener("click", (event) => {
+    document.querySelector("#time").classList.remove("activated");
+    document.querySelector('#time').classList.add("deactivated");
+        document.querySelector("#location").classList.remove("deactivated");
+    document.querySelector('#location').classList.add("activated");
+
+
+})
+
