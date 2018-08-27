1465271a606eb2efc0868598a067e45d42ff7bc1
Gabe Horvat <realgabrielhorvat@gmail.com>
2018-08-27 12:00:50 +0200

    delete

--- app/models/reviewee.rb
+++
@@ -1,3 +0,0 @@
-class Reviewee < ApplicationRecord
-  has_one :review, has_one :user, foreign_key: 'user_id'
-end
--- app/models/reviewer.rb
+++
@@ -1,3 +0,0 @@
-class Reviewer < ApplicationRecord
-  has_one :review, has_one :user, foreign_key: 'user_id'
-end
