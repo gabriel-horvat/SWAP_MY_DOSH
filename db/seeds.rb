8f0d2b6ec52d41043d0966d57fc3cceced992309
Gabe Horvat <realgabrielhorvat@gmail.com>
2018-08-27 12:46:45 +0200

    put seeds file back to 50 times create for each

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ Review.destroy_all
 
 
 
-5.times do |i|
+50.times do |i|
   puts "Creating User ##{i}"
   User.create!(
   name: Faker::Name.name,
@@ -23,7 +23,7 @@ puts "user created!"
 
 locations_array = [["Tirana, Albania"], ["Andorra la Vella, Andorra"], ["Vienna, Austria"], ["Minsk, Belarus"], ["Brussels, Belgium"], ["Sarajevo, Bosnia and Herzegovina"], ["Sofia, Bulgaria"], ["Zagreb, Croatia"], ["Nicosia, Cyprus"], ["Prague, Czech Republic"], ["Copenhagen, Denmark"], ["Tallinn, Estonia"], ["Tórshavn, Faroe Islands"], ["Helsinki, Finland"], ["Paris, France"], ["Berlin, Germany"], ["Gibraltar, Gibraltar"], ["Athens, Greece"], ["St. Peter Port, Guernsey"], ["Budapest, Hungary"], ["Reykjavik, Iceland"], ["Dublin, Ireland"], ["Douglas, Isle of Man"], ["Rome, Italy"], ["Saint Helier, Jersey"], ["Riga, Latvia"], ["Vaduz, Liechtenstein"], ["Vilnius, Lithuania"], ["Luxembourg, Luxembourg"], ["Skopje, Macedonia"], ["Valletta, Malta"], ["Chișinău, Moldova"], ["Monaco, Monaco"], ["Podgorica, Montenegro"], ["Amsterdam, Netherlands"], ["Oslo, Norway"], ["Warsaw, Poland"], ["Lisbon, Portugal"], ["Bucharest, Romania"], ["Moscow, Russia"], ["City of San Marino, San Marino"], ["Belgrade, Serbia"], ["Bratislava, Slovakia"], ["Ljubljana, Slovenia"], ["Madrid, Spain"], ["Stockholm, Sweden"], ["Bern, Switzerland"], ["Kiev, Ukraine"], ["London, United Kingdom"]]
 
-5.times do |i|
+50.times do |i|
   puts "Creating request ##{i}"
   Request.create!(
   request_currency: Faker::Currency.code,
@@ -45,7 +45,7 @@ puts "requests created!"
 
 
 
-5.times do |i|
+50.times do |i|
   puts "Creating reviews ##{i}"
   Review.create!(
   content: Faker::StarWars.quote,
