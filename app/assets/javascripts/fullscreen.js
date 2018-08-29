4a1a6353644d137aef80ca2eab6d3978eef6224f
Michael Wong <mail@michaelwongsilversmith.com>
2018-08-29 17:14:41 +0200

    trying to hide the address bar

--- app/assets/javascripts/fullscreen.js
+++ app/assets/javascripts/fullscreen.js
@@ -1,4 +1,13 @@
-var goFS = document.getElementById("goFS");
-goFS.addEventListener("click", function() {
-    document.body.requestFullscreen();
-}, false);
+// var goFS = document.getElementById("goFS");
+// goFS.addEventListener("click", function() {
+//     document.body.requestFullscreen();
+// }, false);
+
+// When ready...
+window.addEventListener("load", function() {
+    // Set a timeout...
+    setTimeout(function() {
+        // Hide the address bar!
+        window.scrollTo(0, 1);
+    }, 0);
+});
