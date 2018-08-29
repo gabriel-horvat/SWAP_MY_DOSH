8fc2fbf3a08a5d8d2e67d35d52840350518f50af
Michael Wong <mail@michaelwongsilversmith.com>
2018-08-29 18:12:34 +0200

    added missing semicolumn to application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,8 @@
 import "bootstrap";
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
-import { }
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
+import { };
 
 
 
