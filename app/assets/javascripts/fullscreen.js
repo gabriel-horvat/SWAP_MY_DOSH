3da3b7c285a9856a258db0ef8eeda5af0005b726
Michael Wong <mail@michaelwongsilversmith.com>
2018-08-29 17:30:04 +0200

    try 2 for address bar

--- app/assets/javascripts/fullscreen.js
+++ app/assets/javascripts/fullscreen.js
@@ -3,11 +3,9 @@
 //     document.body.requestFullscreen();
 // }, false);
 
-// When ready...
-window.addEventListener("load", function() {
-    // Set a timeout...
-    setTimeout(function() {
-        // Hide the address bar!
-        window.scrollTo(0, 1);
-    }, 0);
-});
+<script>
+  var goFS = document.getElementById("goFS");
+  goFS.addEventListener("click", function() {
+      document.body.requestFullscreen();
+  }, false);
+</script>
