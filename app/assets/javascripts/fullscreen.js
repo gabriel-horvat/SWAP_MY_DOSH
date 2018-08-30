49e7fc5f4c4f041d0e0118f9556beedbbba89e5b
Michael Wong <mail@michaelwongsilversmith.com>
2018-08-30 10:22:16 +0200

    removed fullscreeen js

--- app/assets/javascripts/fullscreen.js
+++ app/assets/javascripts/fullscreen.js
@@ -2,8 +2,3 @@
 // goFS.addEventListener("click", function() {
 //     document.body.requestFullscreen();
 // }, false);
-
-  var goFS = document.getElementById("goFS");
-  goFS.addEventListener("click", function() {
-      document.body.requestFullscreen();
-  }, false);
