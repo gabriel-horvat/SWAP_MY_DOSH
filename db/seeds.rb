3576b36911e2b0871de07429122b70447ea75001
Michael Wong <mail@michaelwongsilversmith.com>
2018-08-30 12:41:19 +0200

    request seed changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,256 @@ Message.destroy_all
 Review.destroy_all
 puts "destroyed all"
 
+
+  User.create!(
+  name: "Christian Sizer",
+  email: "Christian.Sizer@example.com",
+  password: "123456",
+  remote_photo_url: "https://res.cloudinary.com/dfowktpgu/image/upload/v1535623875/Christian_Sizer.png",
+  description: Faker::HarryPotter.quote
+  )
+
+  User.create!(
+  name: "Laura Lindt",
+  email: "Laura.Lindt@example.com",
+  password: "123456",
+  remote_photo_url: "https://res.cloudinary.com/dfowktpgu/image/upload/v1535623875/Laura_Lindt.png",
+  description: Faker::HarryPotter.quote
+  )
+
+  User.create!(
+  name: "Eloise Ng",
+  email: "Eloise.Ng@example.com",
+  password: "123456",
+  remote_photo_url: "https://res.cloudinary.com/dfowktpgu/image/upload/v1535623875/Eloise_Ng.png",
+  description: Faker::HarryPotter.quote
+  )
+
+  User.create!(
+  name: "Marie Moench",
+  email: "Marie.Moench@example.com",
+  password: "123456",
+  remote_photo_url: "https://res.cloudinary.com/dfowktpgu/image/upload/v1535623875/Marie_Moench.png",
+  description: Faker::HarryPotter.quote
+  )
+
+  User.create!(
+  name: "Phillipp Feierabend",
+  email: "Phillipp.Feierabend@example.com",
+  password: "123456",
+  remote_photo_url: "https://res.cloudinary.com/dfowktpgu/image/upload/v1535623876/Phillipp_Feierabend.png",
+  description: Faker::HarryPotter.quote
+  )
+
+  User.create!(
+  name: "Shinta Nozawa",
+  email: "Shinta.Nozawa@example.com",
+  password: "123456",
+  remote_photo_url: "https://res.cloudinary.com/dfowktpgu/image/upload/v1535623876/Shinta_Nozawa.png",
+  description: Faker::HarryPotter.quote
+  )
+
+  User.create!(
+  name: "Anna Faust",
+  email: "Anna.Faust@example.com",
+  password: "123456",
+  remote_photo_url: "https://res.cloudinary.com/dfowktpgu/image/upload/v1535623875/Anna_Faust.png",
+  description: Faker::HarryPotter.quote
+  )
+
+  User.create!(
+  name: "Hannah Karlsson",
+  email: "Hannah.Karlsson@example.com",
+  password: "123456",
+  remote_photo_url: "https://res.cloudinary.com/dfowktpgu/image/upload/v1535623875/Hannah_Karlsson.png",
+  description: Faker::HarryPotter.quote
+  )
+
+  User.create!(
+  name: "Ollie Anibaba",
+  email: "Ollie.Anibaba@example.com",
+  password: "123456",
+  remote_photo_url: "https://res.cloudinary.com/dfowktpgu/image/upload/v1535623876/Ollie_Anibaba.png",
+  description: Faker::HarryPotter.quote
+  )
+
+  User.create!(
+  name: "Liam Peacock",
+  email: "Liam.Peacock@example.com",
+  password: "123456",
+  remote_photo_url: "https://res.cloudinary.com/dfowktpgu/image/upload/v1535623875/Liam_Peacock.png",
+  description: Faker::HarryPotter.quote
+  )
+
+  User.create!(
+  name: "Harriet Woods",
+  email: "Harriet.Woods@example.com",
+  password: "123456",
+  remote_photo_url: "https://res.cloudinary.com/dfowktpgu/image/upload/v1535623875/Harriet_Woods.png",
+  description: Faker::HarryPotter.quote
+  )
+
+  User.create!(
+  name: "Laura Hale",
+  email: "Laura.Hale@example.com",
+  password: "123456",
+  remote_photo_url: "https://res.cloudinary.com/dfowktpgu/image/upload/v1535623876/Laura_Hale.png",
+  description: Faker::HarryPotter.quote
+  )
+
+  User.create!(
+  name: "Connor Rowe",
+  email: "Connor.Rowe@example.com",
+  password: "123456",
+  remote_photo_url: "https://res.cloudinary.com/dfowktpgu/image/upload/v1535623875/Connor_Rowe.png",
+  description: Faker::HarryPotter.quote
+  )
+
+  Request.create!(
+  request_currency: currency_array.sample[0],
+  wanted_currency: "EUR",
+  request_amount: Faker::Number.number(3),
+  location: "Berlin, Germany",
+  start_date: Faker::Date.backward(14),
+  end_date: Faker::Date.forward(100),
+  status: "pending",
+  user: User.where("id = ?", 1)
+  )
+
+  Request.create!(
+  request_currency: currency_array.sample[0],
+  wanted_currency: "EUR",
+  request_amount: Faker::Number.number(3),
+  location: "Berlin, Germany",
+  start_date: Faker::Date.backward(14),
+  end_date: Faker::Date.forward(100),
+  status: "pending",
+  user: User.where("id = ?", 2)
+  )
+
+  Request.create!(
+  request_currency: currency_array.sample[0],
+  wanted_currency: "EUR",
+  request_amount: Faker::Number.number(3),
+  location: "Berlin, Germany",
+  start_date: Faker::Date.backward(14),
+  end_date: Faker::Date.forward(100),
+  status: "pending",
+  user: User.where("id = ?", 3)
+  )
+
+  Request.create!(
+  request_currency: currency_array.sample[0],
+  wanted_currency: "EUR",
+  request_amount: Faker::Number.number(3),
+  location: "Berlin, Germany",
+  start_date: Faker::Date.backward(14),
+  end_date: Faker::Date.forward(100),
+  status: "pending",
+  user: User.where("id = ?", 4)
+  )
+
+  Request.create!(
+  request_currency: currency_array.sample[0],
+  wanted_currency: "EUR",
+  request_amount: Faker::Number.number(3),
+  location: "Berlin, Germany",
+  start_date: Faker::Date.backward(14),
+  end_date: Faker::Date.forward(100),
+  status: "pending",
+  user: User.where("id = ?", 5)
+  )
+
+  Request.create!(
+  request_currency: currency_array.sample[0],
+  wanted_currency: "EUR",
+  request_amount: Faker::Number.number(3),
+  location: l"Berlin, Germany",
+  start_date: Faker::Date.backward(14),
+  end_date: Faker::Date.forward(100),
+  status: "pending",
+  user: User.where("id = ?", 6)
+  )
+
+  Request.create!(
+  request_currency: currency_array.sample[0],
+  wanted_currency: "EUR",
+  request_amount: Faker::Number.number(3),
+  location: "Berlin, Germany",
+  start_date: Faker::Date.backward(14),
+  end_date: Faker::Date.forward(100),
+  status: "pending",
+  user: User.where("id = ?", 7)
+  )
+
+  Request.create!(
+  request_currency: currency_array.sample[0],
+  wanted_currency: "EUR",
+  request_amount: Faker::Number.number(3),
+  location: "Berlin, Germany",
+  start_date: Faker::Date.backward(14),
+  end_date: Faker::Date.forward(100),
+  status: "pending",
+  user: User.where("id = ?", 8)
+  )
+
+  Request.create!(
+  request_currency: currency_array.sample[0],
+  wanted_currency: "EUR",
+  request_amount: Faker::Number.number(3),
+  location: "Berlin, Germany",
+  start_date: Faker::Date.backward(14),
+  end_date: Faker::Date.forward(100),
+  status: "pending",
+  user: User.where("id = ?", 9)
+  )
+
+  Request.create!(
+  request_currency: currency_array.sample[0],
+  wanted_currency: "EUR",
+  request_amount: Faker::Number.number(3),
+  location: "Berlin, Germany",
+  start_date: Faker::Date.backward(14),
+  end_date: Faker::Date.forward(100),
+  status: "pending",
+  user: User.where("id = ?", 10)
+  )
+
+  Request.create!(
+  request_currency: currency_array.sample[0],
+  wanted_currency: "EUR",
+  request_amount: Faker::Number.number(3),
+  location: "Berlin, Germany",
+  start_date: Faker::Date.backward(14),
+  end_date: Faker::Date.forward(100),
+  status: "pending",
+  user: User.where("id = ?", 11)
+  )
+
+  Request.create!(
+  request_currency: currency_array.sample[0],
+  wanted_currency: "EUR",
+  request_amount: Faker::Number.number(3),
+  location: "Berlin, Germany",
+  start_date: Faker::Date.backward(14),
+  end_date: Faker::Date.forward(100),
+  status: "pending",
+  user: User.where("id = ?", 12)
+  )
+
+  Request.create!(
+  request_currency: currency_array.sample[0],
+  wanted_currency: "EUR",
+  request_amount: Faker::Number.number(3),
+  location: "Berlin, Germany",
+  start_date: Faker::Date.backward(14),
+  end_date: Faker::Date.forward(100),
+  status: "pending",
+  user: User.where("id = ?", 13)
+  )
+
+
+
 50.times do |i|
   puts "Creating User ##{i}"
   User.create!(
